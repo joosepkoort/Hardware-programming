f90c8b94d18986e81b819714b040dfc9a543a7f3
Joosep <jkoort@itcollege.ee>
2016-09-04 16:25:52 +0300

    edited file

--- src/main.c
+++ src/main.c
@@ -3,13 +3,13 @@
 #define	BLINK_DELAY_MS	1000
 int	main	(void)
 {
-				/*	set	pin	5	of	PORTB	for	output*/
-				DDRB	|=	_BV(DDA3);
+				/*	set	pin	25	of	PORTA	for	output*/
+				DDRA	|=	_BV(DDA3);
 				while(1)	{
-								/*	set	pin	5	high	to	turn	led	on	*/
+								/*	set	pin	25	high	to	turn	led	on	*/
 								PORTB	|=	_BV(PORTA3);
 								_delay_ms(BLINK_DELAY_MS);
-								/*	set	pin	5	low	to	turn	led	off	*/
+								/*	set	pin	25	low	to	turn	led	off	*/
 								PORTB	&=	~_BV(PORTA3);
 								_delay_ms(BLINK_DELAY_MS);
 				}
