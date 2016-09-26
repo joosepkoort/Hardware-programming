d897169264b8f13db3c1043b70de1465d41de863
Joosep <jkoort@itcollege.ee>
2016-09-26 19:55:51 +0300

    lab03

--- src/hmi_msg.h
+++ src/hmi_msg.h
@@ -3,8 +3,10 @@
 
 
 #define LDC_NOT_OP "LCD not operational"
+#define STUD_NAME "Joosep Koort"
 #define ERR "Error"
 #define ERR_GEN "Program error"
 
+const char *kuud[6] = {"January", "February", "March", "April" , "May", "June"  };
 
 #endif /* _HMI_MSG_H */
