0293221e76334050b17eff87938e30cf7f21a64c
Joosep <jkoort@itcollege.ee>
2016-10-11 17:02:05 +0300

    edeneb

--- src/hmi_msg.h
+++ src/hmi_msg.h
@@ -1,12 +1,24 @@
 #ifndef _HMI_MSG_H_
 #define _HMI_MSG_H_
-
+#include <avr/pgmspace.h>
 
 #define LDC_NOT_OP "LCD not operational"
 #define STUD_NAME "Joosep Koort"
+#define VERSIOON = "Version: %s built on: %s %s\n"
+#define LIBC "avr-libc version: %s\n"
+#define HEX "\"0x%02x\""
+#define CHAR "%c"
+#define TYHIK "\n"
 #define ERR "Error"
 #define ERR_GEN "Program error"
 
-const char *kuud[6] = {"January", "February", "March", "April" , "May", "June"  };
+#define tervitustekst "Enter    Month    name    first    letter    >"
+//const char *kuud[6] = {"January", "February", "March", "April" , "May", "June"  };
+const char kuud[6][10] PROGMEM = {"January", "February", "March", "April" , "May", "June"  };
+const char VER_FW[]  PROGMEM = "Version: %s built on: %s %s\n";
+const char VER_LIBC[]  PROGMEM = "avr-libc version: %s\n";
+
+
 
 #endif /* _HMI_MSG_H */
+
--- src/main.c
+++ src/main.c
@@ -7,24 +7,39 @@
 #include "print_helper.h"
 #include "hmi_msg.h"
 #include <string.h>
-#define BLINK_DELAY_MS 100
+#define BLINK_DELAY_MS 200
+#include "../lib/hd44780_111/hd44780.h"
+#include <avr/pgmspace.h>
+
+
+#define CONSOLE_START "console started"
+//#define VER_LIBC "avr version: " __AVR_LIBC_VERSION_STRING__"\n"
+//#define VER_FW "Version: " GIT_DESCR "Date" __DATE__ "Time" __TIME__
+
+//#define VER_FW ("Version %s", GIT_DESCR )
+
 
 int main (void)
 {
-    /* set pin 3 of PORTA for output*/
+    /*SAMM1 set pin 3 of PORTA for output*/
     DDRA |= _BV(DDA3);
-    /* Init error console as stderr in UART3 and print user code info */
+    /* Init error console as stderr in UART3 and print libc info */
     uart3_init();
     stderr = &uart3_out;
+    /* INIT CLI  (STDIN AND STDOUT) in urt0 and print student name*/
     uart0_init();
     stdout = &uart0_io;
     stdin = &uart0_io;
-    fprintf(stderr, "Version: %s built on: %s %s\n",
-            GIT_DESCR, __DATE__, __TIME__);
-    fprintf(stderr, "avr-libc version: %s\n", __AVR_LIBC_VERSION_STRING__);
-    /*End UART3 init and info print */
-    fprintf(stdout, "%s \n", STUD_NAME);
+    lcd_init();
+    lcd_clrscr();
+    lcd_puts(STUD_NAME);
+    //
+    fprintf_P(stdout, VER_LIBC,  GIT_DESCR, __DATE__, __TIME__);
+    fprintf_P(stdout,VER_FW, __AVR_LIBC_VERSION_STRING__);
+    /*PRINT STUDENT NAME */
+    fprintf_P(stdout, STUD_NAME);
     print_ascii_tbl(stdout);
+    //LAB03.1 print ASCII maps to CLI
     unsigned char test2 [128] = {0};
 
     for (unsigned char i = 0; i < 128; i++) {
@@ -33,17 +48,46 @@ int main (void)
 
     print_for_human(stdout, test2, 128);
     char inBuf = 0;
+    //init lcd and display student name
 
     while (1) {
-        fprintf(stdout,    "Enter    Month    name    first    letter    >");
+        //set blinK LED ON
+        PORTA |= _BV(PORTA3);
+        _delay_ms(BLINK_DELAY_MS);
+        //ask user to input first letter of month name
+        fprintf(stdout, tervitustekst);
         fscanf(stdin,    "%c",    &inBuf);
         fprintf(stdout,    "%c\n",    inBuf);
+        //try to find month beginning with letter from list
 
         for    (int    i    =    0;    i    <    6;    i++)    {
-            if    (!strncmp(strupr(&inBuf),    kuud[i],    1))    {
+            if    (!strncmp(&inBuf,    kuud[i],    1))    {
+                //kui leiab vaste siis:
+                //prindib konsooli
                 fprintf(stdout,    "%s\n",    kuud[i]);
+                //ja LCD teisele reale
+                lcd_goto(0x40);
+                lcd_puts("              ");
+                lcd_goto(0x40);
+                lcd_puts(kuud[i]);
+  
+
+
+            
+
+            
+            } else if  (strncmp(&inBuf,    kuud[i],    1)) {
+                //teeb LCD teise rea tühjaks
+                //
+                lcd_goto(0x40);
+                lcd_puts("               ");
             }
+
+            //lülitab LEDi välja
+            PORTA &= ~_BV(PORTA3);
+            _delay_ms(BLINK_DELAY_MS);
         }
     }
 }
+
 
