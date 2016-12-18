814037ad9238a99fbb31ba67f24bbd205b0a1a93
Joosep <jkoort@itcollege.ee>
2016-12-18 14:30:29 +0200

    minor changes

--- src/cli_microrl.h
+++ src/cli_microrl.h
@@ -1,21 +1,21 @@
 /*
     Copyright 2016 Joosep Koort
-    
-    This file is part of lab06_Joosep_Koort.
 
-    lab06_Joosep_Koort is free software: you can redistribute it and/or modify
+    This file is part of riistvara_lahedane_programmeerimine_Joosep_Koort.
+
+    riistvara_lahedane_programmeerimine_Joosep_Koort is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    lab06_Joosep_Koort is distributed in the hope that it will be useful,
+    riistvara_lahedane_programmeerimine_Joosep_Koort is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with lab06_Joosep_Koort.  If not, see <http://www.gnu.org/licenses/>.
-    
+    along with riistvara_lahedane_programmeerimine_Joosep_Koort.  If not, see <http://www.gnu.org/licenses/>.
+
 */
 
 #ifndef _CLI_MICROCL_H_
